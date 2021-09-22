36c37cd2adeeff70e0c706879ae090bff3158f09
Neil B. Chanez Gracia <Chanez89@Hotmail.com>
2021-09-21 21:45:23 -0700

    commit tree d4f02c9b339cb171b8805a55dd869bb390b86d8c

---
+++ src/components/Links.js
@@ -0,0 +1,10 @@
+
+
+function Links(){
+    return(
+        <h3>Links</h3>,
+        <a href="https://github.com/liza">Github link</a>,
+        <a href="https://www.linkedin.com/in/liza/">Linkedin link</a>
+    );
+}
+export default Links;
